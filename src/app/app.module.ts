1109664b030e5e1d006c7a834e34a0b65e606349
Kirill <dolganovk.exceedteam@gmail.com>
2022-11-11 22:02:47 +0300

    Create main design for crypto daily news

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftSideBarComponent } from './components/left-side-bar/left-side-bar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftSideBarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
