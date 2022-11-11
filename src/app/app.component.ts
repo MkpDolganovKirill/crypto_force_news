4f12b35f0b8ea05abc7cadb9deedd2c454d68ae6
Kirill <dolganovk.exceedteam@gmail.com>
2022-11-11 03:43:08 +0300

    Delete third party changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'crypto_force_news';
-}
+export class AppComponent {}
