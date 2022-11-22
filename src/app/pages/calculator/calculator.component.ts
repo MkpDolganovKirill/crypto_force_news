e4a881f2b14a6f2c8825ef0ca58406dbb6c10885
Kirill <dolganovk.exceedteam@gmail.com>
2022-11-22 21:57:43 +0300

    calculator page and functionality

--- src/app/pages/calculator/calculator.component.ts
+++ src/app/pages/calculator/calculator.component.ts
@@ -6,7 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.scss'],
 })
 export class CalculatorComponent implements OnInit {
-  constructor() {}
+  public fieldsArray = [[0, 0, 0], [0, 0, 0]];
+  public average = 0;
+  public totalCount = 0;
+  public totalSum = 0;
 
-  ngOnInit(): void {}
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  removeLastField() {
+    if (this.fieldsArray.length <= 2) return;
+    this.fieldsArray.pop();
+  }
+
+
+  addNewField() {
+    this.fieldsArray.push([0, 0, 0])
+  }
+
+  refreshValues() {
+    this.totalSum = 0;
+    this.totalCount = 0;
+    this.fieldsArray.forEach(element => {
+      element[2] = element[0] * element[1];
+      this.totalCount += element[1];
+      this.totalSum += element[2];
+    })
+    this.average = this.totalSum / this.totalCount;
+    console.log(this.fieldsArray);
+  }
 }
