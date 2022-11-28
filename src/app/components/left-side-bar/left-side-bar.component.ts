20fabc9bde8031be24c9a008c386fffc29c6c044
nnabse <nnabse@gmail.com>
2022-11-28 14:31:53 +0300

    add get top 10 popylar coins feature

--- src/app/components/left-side-bar/left-side-bar.component.ts
+++ src/app/components/left-side-bar/left-side-bar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ResourceService } from '@services/resource.service';
+import { StoreService } from '@services/store.service';
+import { CryptoItem } from '@pages/crypto-rates/interfaces';
+import { PageName } from '@enums/pageName.enums';
 
 @Component({
   selector: 'app-left-side-bar',
@@ -6,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-side-bar.component.scss'],
 })
 export class LeftSideBarComponent implements OnInit {
-  constructor() {}
+  public pageName = PageName;
+  public cryptoList: CryptoItem[] = [];
 
-  ngOnInit(): void {}
+  constructor(private store: StoreService, public resource: ResourceService) {}
+
+  ngOnInit(): void {
+    this.store.cryptoList$.subscribe((resp) => {
+      if (!resp) return;
+      this.cryptoList = resp;
+      this.cryptoList = this.cryptoList.slice(0, 10);
+    });
+  }
 }
